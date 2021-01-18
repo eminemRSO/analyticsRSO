ddd73ed7c8d88fe3862218097883292715859686
M <m@bass.si>
2021-01-18 07:27:14 +0100

    extra tasksa ingress deployment

--- src/main/java/me/eminem/analyticsRSO/StorageController.java
+++ src/main/java/me/eminem/analyticsRSO/StorageController.java
@@ -1,7 +1,9 @@
 package me.eminem.analyticsRSO;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.stream.Stream;
@@ -20,8 +22,20 @@ public class StorageController {
 
 
 
-    @GetMapping(path="/tasks")
-    public Stream<TaskDTO> getAllTasks(){
-        return taskService.getAllTasks().stream().map(task -> modelMapper.map(task,TaskDTO.class));
+    @CrossOrigin(origins = "*")
+    @GetMapping(path="/emptyCall")
+    public void emptyCall(){
+        taskService.emptyCall();
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping(path="/returnNumber")
+    public String returnNumber(){
+        return taskService.returnNumber();
+    }
+
+    @GetMapping(path="/sleep")
+    public String sleep() throws InterruptedException{
+        return taskService.sleep();
     }
 }
--- src/main/java/me/eminem/analyticsRSO/TaskService.java
+++ src/main/java/me/eminem/analyticsRSO/TaskService.java
@@ -1,6 +1,8 @@
 package me.eminem.analyticsRSO;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -8,11 +10,25 @@ import java.util.List;
 public class TaskService {
     TaskRepository taskRepository;
 
+    private int stKlicev = 0;
+
     public TaskService(TaskRepository taskRepository) {
         this.taskRepository = taskRepository;
     }
 
     List<Task> getAllTasks(){
         return taskRepository.findAll();
+    }
+
+    void emptyCall(){
+        this.stKlicev++;
+    }
+    String returnNumber(){
+        return ""+this.stKlicev;
+    }
+
+    String sleep() throws InterruptedException{
+        Thread.sleep(5000);
+        return "Končal sem z obsežno operacijo";
     }
 }
